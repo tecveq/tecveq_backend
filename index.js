83596da20e47a77ec6f3821256d45101b982e3f8
muneeb-jutt <sp21-bcs-072@isbstudent.comsats.edu.pk>
2024-08-24 07:50:54 +0500

    count changes

--- index.js
+++ index.js
@@ -90,8 +90,8 @@ app.get("/", (req, res) => {
   // res.sendFile(path.resolve(__dirname, "public", "index.html"));
   return res.send({
     success: true,
-    lastCount:3,
-    count: 4,
+    lastCount:5,
+    count: 6,
     message: "Backend live on AWS!"
   })
 });
