1b899121e53b51c5ee2183f6dd500b51f4a89720
HamzaChaudhary786 <hmzsattar99@gmail.com>
2024-12-04 23:28:23 -0800

    add production url

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use(
   cors({
     credentials: true,
     origin: isProduction
-      ? ["https://tca-frontend-sync-git-main-zees-projects-3a466cf5.vercel.app", "*"]  // Production URLs only
+      ? ["https://tca-frontend-sync-git-main-zees-projects-3a466cf5.vercel.app"]  // Production URLs only
       : ["http://localhost:5173", "*"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
